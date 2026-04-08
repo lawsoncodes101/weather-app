3d798397ac848ae244af9967d15231e0a6557139
lawson101 <lawsoncodes101@gmail.com>
2026-04-08 02:18:39 +0100

    Added webpack dependencies

---
+++ webpack.dev.js
@@ -0,0 +1,11 @@
+import { merge } from "webpack-merge";
+import config from "./webpack.config.js";
+
+export default merge(config, {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: {
+        static: "./dist",
+        watchFiles: ["./src/**/*"],
+    },
+});
