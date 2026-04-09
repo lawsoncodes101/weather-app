22a549a8367eda4c0d805aa129575e3e7655300e
lawson101 <lawsoncodes101@gmail.com>
2026-04-09 17:08:56 +0100

    Added try...catch for tracking errors

--- src/modules/weatherApi.js
+++ src/modules/weatherApi.js
@@ -1,15 +1,35 @@
-const BASE_URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+const BASE_URL =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
 const params = new URLSearchParams({
     unitGroup: "us",
     key: "U4LFZMCQF4663KVW9UQ4DPUC4",
     contentType: "json",
 });
 
-export async function fetchData() {
-    const response = await fetch(
-        `${BASE_URL}${encodeURIComponent("Nigeria, Abuja")}?${params}`,
-    );
-    const data = await response.json();
+export async function fetchData(query) {
+    try {
+        if (!(typeof query === "string")) {
+            throw new TypeError("Expected a string");
+        };
 
-    console.log(data);
+        const res = await fetch(
+            `${BASE_URL}${encodeURIComponent(query)}/today?${params}`,
+        );
+
+        if (!res.ok) {
+            throw new Error("")
+        }
+        const data = await res.json();
+        console.log(data);
+
+        return {
+            date: new Date().toISOString().split("T")[0],
+            address: data.resolvedAddress,
+            timezone: data.timezone,
+            condition: data.currentConditions.conditions,
+            snow: data.currentConditions.snow,
+        };
+    } catch (err) {
+        console.error(err);
+    }
 }
--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
 import { fetchData } from "./modules/weatherApi.js";
 
-await fetchData();
+const form = document.querySelector("form");
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    try {
+        const data = await fetchData("Nigeria, Lagos");
+        console.log(data);
+    } catch (err) {
+        console.error(err);
+    }
+});
